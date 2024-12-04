70dca400973798d3ece35a911ea974e5e138407e
ibado <ignaciobado.unc@gmail.com>
2024-12-04 20:26:02 +0100

    Add Go solutions for day 4

---
+++ go/day4.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"iter"
+)
+
+var WORD1 = []byte{'X', 'M', 'A', 'S'}
+var WORD2 = []byte{'M', 'A', 'S'}
+
+func day4Part1(lines iter.Seq[string]) int {
+	mx := parseMatrix(lines)
+	count := 0
+	for i := 0; i < len(mx); i++ {
+		for j := 0; j < len(mx[0]); j++ {
+			count += searchPart1(mx, i, j)
+		}
+	}
+	return count
+}
+
+func day4Part2(lines iter.Seq[string]) int {
+	mx := parseMatrix(lines)
+	count := 0
+	for i := 0; i < len(mx)-2; i++ {
+		for j := 0; j < len(mx[0])-2; j++ {
+			c := 0
+			c += searchij(mx, WORD2, i, j, incr, incr)
+			c += searchij(mx, WORD2, i, j+2, incr, decr)
+			c += searchij(mx, WORD2, i+2, j, decr, incr)
+			c += searchij(mx, WORD2, i+2, j+2, decr, decr)
+			if c == 2 {
+				count++
+			}
+		}
+	}
+	return count
+}
+
+func parseMatrix(lines iter.Seq[string]) [][]byte {
+	var mx [][]byte
+	for l := range lines {
+		mx = append(mx, []byte(l))
+	}
+	return mx
+}
+
+func searchPart1(mx [][]byte, i int, j int) int {
+	count := 0
+	// search down
+	count += searchij(mx, WORD1, i, j, incr, idle)
+	// search right
+	count += searchij(mx, WORD1, i, j, idle, incr)
+	// search up
+	count += searchij(mx, WORD1, i, j, decr, idle)
+	// search left
+	count += searchij(mx, WORD1, i, j, idle, decr)
+	// search diagonally right down
+	count += searchij(mx, WORD1, i, j, incr, incr)
+	// search diagonally left up
+	count += searchij(mx, WORD1, i, j, decr, decr)
+	// search diagonally right up
+	count += searchij(mx, WORD1, i, j, decr, incr)
+	// search diagonally left down
+	count += searchij(mx, WORD1, i, j, incr, decr)
+	return count
+}
+
+func searchij(
+	mx [][]byte,
+	word []byte,
+	i int, j int,
+	ifunc func(int, int) int,
+	jfunc func(int, int) int,
+) int {
+	wlen := len(word)
+	mxlen := len(mx)
+	for k := 0; k < wlen; k++ {
+		ii := ifunc(i, k)
+		jj := jfunc(j, k)
+		if ii < 0 || jj < 0 || ii >= mxlen || jj >= mxlen || mx[ii][jj] != word[k] {
+			return 0
+		}
+	}
+	return 1
+}
+
+func incr(i, inc int) int {
+	return i + inc
+}
+
+func decr(i, dec int) int {
+	return i - dec
+}
+
+func idle(i, inc int) int {
+	return i
+}
--- go/main.go
+++ go/main.go
@@ -13,4 +13,7 @@ func main() {
 
 	fmt.Printf("day3 part1: %d\n", day3Part1(readLines(3)))
 	fmt.Printf("day3 part2: %d\n", day3Part2(readLines(3)))
+
+	fmt.Printf("day4 part1: %d\n", day4Part1(readLines(4)))
+	fmt.Printf("day4 part2: %d\n", day4Part2(readLines(4)))
 }

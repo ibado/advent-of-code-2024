e7264a250d5aa7ef8426e6bf078127281aa0975d
ibado <ignaciobado.unc@gmail.com>
2024-12-28 15:49:04 +0100

    Optimize day 7 part 2 with caching and goroutines

--- go/day7.go
+++ go/day7.go
@@ -4,10 +4,16 @@ import (
 	"iter"
 	"slices"
 	"strconv"
+	"sync"
 )
 
 var operators1 = []byte{'+', '*'}
 var operators2 = []byte{'+', '*', 'c'}
+
+type Day7Input struct {
+	nums   []int
+	target int
+}
 
 func day7Part1(lines iter.Seq[string]) int {
 	return solve(operators1, lines)
@@ -17,33 +23,59 @@ func day7Part2(lines iter.Seq[string]) int {
 	return solve(operators2, lines)
 }
 
-// TODO: use goroutines to split the work, spetially for part 2
 func solve(ops []byte, lines iter.Seq[string]) int {
-	sum := 0
-	for l := range lines {
-		n := parseNums([]byte(l))
-		target := n[0]
-		nums := n[1:]
-		opsComb := generateOps(ops, len(nums)-1)
-	LOOP:
-		for _, ops := range opsComb {
-			result := nums[0]
-			for i, n := range nums[1:] {
-				switch ops[i] {
-				case '+':
-					result += n
-				case '*':
-					result *= n
-				case 'c':
-					result = concatOp(result, n)
+	m := make(map[int][][]byte)
+	input := parseDay7(lines)
+	for _, in := range input {
+		nums := in.nums
+		var opsPerm [][]byte
+		if val, ok := m[len(nums)]; ok {
+			opsPerm = val
+		} else {
+			opsPerm = genPermutations(ops, len(nums))
+			m[len(nums)] = opsPerm
+		}
+	}
+
+	sumCh := make(chan int)
+	var wg sync.WaitGroup
+	for _, in := range input {
+		wg.Add(1)
+		go func() {
+			target := in.target
+			nums := in.nums
+			opsPerm := m[len(nums)]
+		LOOP:
+			for _, ops := range opsPerm {
+				result := nums[0]
+				for i, n := range nums[1:] {
+					switch ops[i] {
+					case '+':
+						result += n
+					case '*':
+						result *= n
+					case 'c':
+						result = concatOp(result, n)
+					}
+				}
+				if result == target {
+					sumCh <- target
+					break LOOP
 				}
 			}
-			if result == target {
-				sum += target
-				break LOOP
-			}
-		}
 
+			wg.Done()
+		}()
+	}
+
+	go func() {
+		wg.Wait()
+		close(sumCh)
+	}()
+
+	sum := 0
+	for s := range sumCh {
+		sum += s
 	}
 	return sum
 }
@@ -56,20 +88,33 @@ func concatOp(a, b int) int {
 	return n
 }
 
-func generateOps(ops []byte, n int) [][]byte {
+func genPermutations(ops []byte, n int) [][]byte {
+	var rec func(ops []byte, prefix []byte, res *[][]byte, n int)
+
+	rec = func(ops []byte, prefix []byte, res *[][]byte, n int) {
+		if n == 1 {
+			*res = append(*res, slices.Clone(prefix))
+			return
+		}
+
+		for _, op := range ops {
+			np := append(prefix, op)
+			rec(ops, np, res, n-1)
+		}
+	}
+
 	var res [][]byte
-	generateOpsRec(ops, []byte{}, &res, n)
+	rec(ops, []byte{}, &res, n)
 	return res
 }
 
-func generateOpsRec(ops []byte, prefix []byte, res *[][]byte, n int) {
-	if n == 0 {
-		*res = append(*res, slices.Clone(prefix))
-		return
+func parseDay7(lines iter.Seq[string]) []Day7Input {
+	var res []Day7Input
+	for l := range lines {
+		n := parseNums([]byte(l))
+		target := n[0]
+		nums := n[1:]
+		res = append(res, Day7Input{nums, target})
 	}
-
-	for _, op := range ops {
-		np := append(prefix, op)
-		generateOpsRec(ops, np, res, n-1)
-	}
+	return res
 }

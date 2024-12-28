bc4833ff59146be418668ab4ea005f1d11dea206
ibado <ignaciobado.unc@gmail.com>
2024-12-28 16:45:23 +0100

    Add run func to print time for each day/part

--- go/main.go
+++ go/main.go
@@ -2,63 +2,69 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
+func run(msg string, result func() any) {
+	start := time.Now()
+	fmt.Println(msg, result(), "-", "time:", time.Since(start))
+}
+
 func main() {
-	fmt.Printf("day1 part1: %d\n", day1Part1(readLines(1)))
-	fmt.Printf("day1 part2: %d\n", day1Part2(readLines(1)))
+	run("day1 part1:", func() any { return day1Part1(readLines(1)) })
+	run("day1 part2:", func() any { return day1Part2(readLines(1)) })
 
-	fmt.Printf("day2 part1: %d\n", day2Part1(readLines(2)))
-	fmt.Printf("day2 part2: %d\n", day2Part2(readLines(2)))
+	run("day2 part1:", func() any { return day2Part1(readLines(2)) })
+	run("day2 part2:", func() any { return day2Part2(readLines(2)) })
 
-	fmt.Printf("day3 part1: %d\n", day3Part1(readLines(3)))
-	fmt.Printf("day3 part2: %d\n", day3Part2(readLines(3)))
+	run("day3 part1:", func() any { return day3Part1(readLines(3)) })
+	run("day3 part2:", func() any { return day3Part2(readLines(3)) })
 
-	fmt.Printf("day4 part1: %d\n", day4Part1(readLines(4)))
-	fmt.Printf("day4 part2: %d\n", day4Part2(readLines(4)))
+	run("day4 part1:", func() any { return day4Part1(readLines(4)) })
+	run("day4 part2:", func() any { return day4Part2(readLines(4)) })
 
-	fmt.Printf("day5 part1: %d\n", day5Part1(readLines(5)))
-	fmt.Printf("day5 part2: %d\n", day5Part2(readLines(5)))
+	run("day5 part1:", func() any { return day5Part1(readLines(5)) })
+	run("day5 part2:", func() any { return day5Part2(readLines(5)) })
 
-	fmt.Printf("day6 part1: %d\n", day6Part1(readLines(6)))
-	fmt.Printf("day6 part2: %d\n", day6Part2(readLines(6)))
+	run("day6 part1:", func() any { return day6Part1(readLines(6)) })
+	run("day6 part2:", func() any { return day6Part2(readLines(6)) })
 
-	fmt.Printf("day7 part1: %d\n", day7Part1(readLines(7)))
-	fmt.Printf("day7 part2: %d\n", day7Part2(readLines(7)))
+	run("day7 part1:", func() any { return day7Part1(readLines(7)) })
+	run("day7 part2:", func() any { return day7Part2(readLines(7)) })
 
-	fmt.Printf("day8 part1: %d\n", day8Part1(readLines(8)))
-	fmt.Printf("day8 part2: %d\n", day8Part2(readLines(8)))
+	run("day8 part1:", func() any { return day8Part1(readLines(8)) })
+	run("day8 part2:", func() any { return day8Part2(readLines(8)) })
 
-	fmt.Printf("day9 part1: %d\n", day9Part1(readLines(9)))
-	fmt.Printf("day9 part2: %d\n", day9Part2(readLines(9)))
+	run("day9 part1:", func() any { return day9Part1(readLines(9)) })
+	run("day9 part2:", func() any { return day9Part2(readLines(9)) })
 
-	fmt.Printf("day10 part1: %d\n", day10Part1(readLines(10)))
-	fmt.Printf("day10 part2: %d\n", day10Part2(readLines(10)))
+	run("day10 part1:", func() any { return day10Part1(readLines(10)) })
+	run("day10 part2:", func() any { return day10Part2(readLines(10)) })
 
-	fmt.Printf("day11 part1: %d\n", day11Part1(readLines(11)))
-	fmt.Printf("day11 part2: %d\n", day11Part2(readLines(11)))
+	run("day11 part1:", func() any { return day11Part1(readLines(11)) })
+	run("day11 part2:", func() any { return day11Part2(readLines(11)) })
 
-	fmt.Printf("day12 part1: %d\n", day12Part1(readLines(12)))
-	fmt.Printf("day12 part2: %d\n", day12Part2(readLines(12)))
+	run("day12 part1:", func() any { return day12Part1(readLines(12)) })
+	run("day12 part2:", func() any { return day12Part2(readLines(12)) })
 
-	fmt.Printf("day13 part1: %d\n", day13Part1(readLines(13)))
-	fmt.Printf("day13 part2: %d\n", day13Part2(readLines(13)))
+	run("day13 part1:", func() any { return day13Part1(readLines(13)) })
+	run("day13 part2:", func() any { return day13Part2(readLines(13)) })
 
-	fmt.Printf("day14 part1: %d\n", day14Part1(readLines(14)))
-	fmt.Printf("day14 part2: %d\n", day14Part2(readLines(14)))
+	run("day14 part1:", func() any { return day14Part1(readLines(14)) })
+	run("day14 part2:", func() any { return day14Part2(readLines(14)) })
 
-	fmt.Printf("day15 part1: %d\n", day15Part1(readLines(15)))
-	fmt.Printf("day15 part2: %d\n", day15Part2(readLines(15)))
+	run("day15 part1:", func() any { return day15Part1(readLines(15)) })
+	run("day15 part2:", func() any { return day15Part2(readLines(15)) })
 
-	fmt.Printf("day16 part1: %d\n", day16Part1(readLines(16)))
-	fmt.Printf("day16 part2: %d\n", day16Part2(readLines(16)))
+	run("day16 part1:", func() any { return day16Part1(readLines(16)) })
+	run("day16 part2:", func() any { return day16Part2(readLines(16)) })
 
-	fmt.Printf("day17 part1: %s\n", day17Part1(readLines(17)))
-	fmt.Printf("day17 part2: %d\n", day17Part2(readLines(17)))
+	run("day17 part1:", func() any { return day17Part1(readLines(17)) })
+	run("day17 part2:", func() any { return day17Part2(readLines(17)) })
 
-	fmt.Printf("day18 part1: %d\n", day18Part1(readLines(18)))
-	fmt.Printf("day18 part2: %d\n", day18Part2(readLines(18)))
+	run("day18 part1:", func() any { return day18Part1(readLines(18)) })
+	run("day18 part2:", func() any { return day18Part2(readLines(18)) })
 
-	fmt.Printf("day19 part1: %d\n", day19Part1(readLines(19)))
-	fmt.Printf("day19 part2: %d\n", day19Part2(readLines(19)))
+	run("day19 part1:", func() any { return day19Part1(readLines(19)) })
+	run("day19 part2:", func() any { return day19Part2(readLines(19)) })
 }

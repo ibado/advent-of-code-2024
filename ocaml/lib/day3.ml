06b7882bf77a2e8ad8673642452be3df10c062fc
ibado <ignaciobado.unc@gmail.com>
2025-11-20 08:36:51 +0100

    Add Ocaml solution for day3 part2

--- ocaml/lib/day3.ml
+++ ocaml/lib/day3.ml
@@ -1,16 +1,33 @@
+let product groups =
+  int_of_string (Re.Group.get groups 1) * int_of_string (Re.Group.get groups 2)
+
 let part1 input =
   let regexp = Re.Pcre.re {|mul\((\d+),(\d+)\)|} |> Re.compile in
   let rec muls ?(pos = 0) ?(acc = 0) s =
     match Re.exec_opt ~pos regexp s with
-    | Some groups ->
-        let sum =
-          int_of_string (Re.Group.get groups 1)
-          * int_of_string (Re.Group.get groups 2)
-        in
-        muls s ~pos:(Re.Group.stop groups 0) ~acc:(acc + sum)
+    | Some group ->
+        muls s ~pos:(Re.Group.stop group 0) ~acc:(acc + product group)
     | None -> acc
   in
 
   List.fold_left (fun acc it -> acc + muls it) 0 input
 
-let part2 _input = 0
+let part2 input =
+  let regexp =
+    Re.Pcre.re {|mul\((\d+),(\d+)\)|don't\(\)|do\(\)|} |> Re.compile
+  in
+  let rec muls ?(pos = 0) acc s =
+    match Re.exec_opt ~pos regexp s with
+    | Some group ->
+        let sum, enabled = acc in
+        let new_acc =
+          match Re.Group.get group 0 with
+          | "do()" -> (sum, true)
+          | "don't()" -> (sum, false)
+          | _ -> if enabled then (sum + product group, enabled) else acc
+        in
+        muls ~pos:(Re.Group.stop group 0) new_acc s
+    | None -> acc
+  in
+
+  List.fold_left (fun acc it -> muls acc it) (0, true) input |> fst

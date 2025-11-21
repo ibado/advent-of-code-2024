998a9ecb6b23fa48a27df58b1642615e585ec3e9
ibado <ignaciobado.unc@gmail.com>
2025-11-21 16:01:37 +0100

    Add Ocaml solution for day 4 part 1

--- ocaml/bin/main.ml
+++ ocaml/bin/main.ml
@@ -2,14 +2,14 @@ open Printf
 open Aoc2024
 open Aoc2024.Util;;
 
-input_day 1 |> Day1.part1 |> printf "part1: %d\n";
-input_day 1 |> Day1.part2 |> printf "part2: %d\n";
+input_day 1 |> Day1.part1 |> printf "day1 part1: %d\n";
+input_day 1 |> Day1.part2 |> printf "day1 part2: %d\n\n";
 
-input_day 2 |> Day2.part1 |> printf "part1: %d\n";
-input_day 2 |> Day2.part2 |> printf "part2: %d\n";
+input_day 2 |> Day2.part1 |> printf "day2 part1: %d\n";
+input_day 2 |> Day2.part2 |> printf "day2 part2: %d\n\n";
 
-input_day 3 |> Day3.part1 |> printf "part1: %d\n";
-input_day 3 |> Day3.part2 |> printf "part2: %d\n";
+input_day 3 |> Day3.part1 |> printf "day3 part1: %d\n";
+input_day 3 |> Day3.part2 |> printf "day3 part2: %d\n\n";
 
-input_day 4 |> Day4.part1 |> printf "part1: %d\n";
-input_day 4 |> Day4.part2 |> printf "part2: %d\n"
+input_day 4 |> Day4.part1 |> printf "day4 part1: %d\n";
+input_day 4 |> Day4.part2 |> printf "day4 part2: %d\n\n"
---
+++ ocaml/lib/day4.ml
@@ -0,0 +1,47 @@
+type point = { x : int; y : int }
+
+let dirs : point array =
+  [|
+    { x = -1; y = -1 };
+    { x = -1; y = 0 };
+    { x = -1; y = 1 };
+    { x = 1; y = -1 };
+    { x = 1; y = 0 };
+    { x = 1; y = 1 };
+    { x = 0; y = -1 };
+    { x = 0; y = 1 };
+  |]
+
+let point_add p1 p2 = { x = p1.x + p2.x; y = p1.y + p2.y }
+let point_in_range r p = p.x >= 0 && p.x < r.x && p.y >= 0 && p.y < r.y
+
+let part1 input =
+  let mx =
+    Array.of_list input |> Array.map String.to_seq |> Array.map Array.of_seq
+  in
+  let rows = Array.length mx in
+  let cols = Array.length mx.(0) in
+  let range_point = { x = rows; y = cols } in
+  let count = ref 0 in
+  let is_xmas p0 dir mx =
+    let p1 = point_add p0 dir in
+    let p2 = point_add p1 dir in
+    let p3 = point_add p2 dir in
+    point_in_range range_point p3
+    && mx.(p0.x).(p0.y) = 'X'
+    && mx.(p1.x).(p1.y) = 'M'
+    && mx.(p2.x).(p2.y) = 'A'
+    && mx.(p3.x).(p3.y) = 'S'
+  in
+  for i = 0 to rows - 1 do
+    for j = 0 to cols - 1 do
+      for di = 0 to Array.length dirs - 1 do
+        let p = { x = i; y = j } in
+        let d = dirs.(di) in
+        if is_xmas p d mx then count := !count + 1
+      done
+    done
+  done;
+  !count
+
+let part2 _input = 0
